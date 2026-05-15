ee194b32dfff2ddaa4fb26b748540b9f9f40facd
luolin1984 <35947285+luolin1984@users.noreply.github.com>
2026-05-15 20:25:11 +0800

    Delete comsol directory

--- comsol/export_results.m
+++
@@ -1,6 +0,0 @@
-% Export COMSOL field, temperature, and summary outputs.
-
-function export_results()
-    error("export_results:notImplemented", ...
-        "Define export probes/tables and write outputs to results/raw_comsol_exports.");
-end
