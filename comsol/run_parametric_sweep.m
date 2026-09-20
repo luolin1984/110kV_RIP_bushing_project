99facf1db010c8c94a4f80484cdb8129ec6cd112
luolin1984 <35947285+luolin1984@users.noreply.github.com>
2026-09-20 13:22:16 +0800

    Delete comsol directory

--- comsol/run_parametric_sweep.m
+++
@@ -1,6 +0,0 @@
-% Run COMSOL parametric sweeps for operating cases.
-
-function run_parametric_sweep()
-    error("run_parametric_sweep:notImplemented", ...
-        "Connect this script to base_model.mph and data/processed/operating_cases.csv.");
-end
